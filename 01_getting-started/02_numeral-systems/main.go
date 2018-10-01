b63eff48bd857f49a2073b376c712b4bc26da9b1
mbzama <zamamb@gmail.com>
2018-10-01 15:27:59 +0530

    Add section 1 and 2

---
+++ 01_getting-started/02_numeral-systems/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Decimal: %d - Hexa: %x \n", 42, 42)
+}
---
+++ 02_package/main/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mbzama/go-lang-training2/02_package/stringutil"
+)
+
+func main() {
+	fmt.Printf(stringutil.Reverse("oG olleH"))
+}
